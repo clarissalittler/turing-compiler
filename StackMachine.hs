4c668eb7867db95373eda72e6adbc32891c05dfb
Clarissa Littler <clarissa.littler@gmail.com>
2014-03-06 17:52:20 -0800

    finished stackmachine interpreter and compilation from AST to stack machine.

--- StackMachine.hs
+++ StackMachine.hs
@@ -3,11 +3,13 @@ module StackMachine where
 import Data.Map (Map)
 import qualified Data.Map as M
 import Control.Monad.State
+import Data.Maybe
+import Data.Either
 
 {- In this file we're going to introduce the linear stack machine and its semantics that we're going to use as the intermediate language for our compiler to a Turing machine. Why? Because linear representations and a stack are easier to emulate with a TM than an actual register machine. -}
 
 type Var = Int
-type StackM = State ([Either Int Var],Map Var Int, [Inst])
+type StackM = State ([Either Int Var],Map Var Int)
 
 data Inst = Push (Either Int Var)
             | Add
@@ -20,22 +22,22 @@ data Inst = Push (Either Int Var)
             | Set Var
             | Not
             | If
-            | While Int
+            | While [Inst] {- Test -}  [Inst] {- Body -}
 {- how do I do While loops in a stack machine? -}
 
 pop :: StackM Int 
 pop = do
-  (stack, vs, is)  <- get
-  put $ (tail stack, vs, is)
+  (stack, vs)  <- get
+  put $ (tail stack, vs)
   case (head stack) of
     Left i -> return i
     Right v -> return $ fromJust $ M.lookup v vs
 
 push :: Either Int Var -> StackM ()
-push v = modify (\ (s,vars,i) -> (v : s,vars i))
+push v = modify (\ (s,vars) -> (v : s,vars))
 
 update :: Var -> Int -> StackM ()
-update v i = modify (\ (s,vars,i) -> (s, M.insert v i vars, i))
+update v i = modify (\ (s,vars) -> (s, M.insert v i vars))
 
 toBool :: Int -> Bool
 toBool i = i > 0
@@ -75,17 +77,27 @@ evalInst Not = do
 evalInst (Set v) = do
   x <- pop
   update x v
-evalInst (While i) = undefined
-evalInst Pop = pop >> return ()
+evalInst (While testInsts bodyInsts) = execLoop testInsts bodyInsts
+evalInst Pop = pop >> return () {- we might not actually want this in here since it's not "accesible" per se from the language, but it also doesn't really matter -}
 evalInst (Push v) = push v
-
+                    
+execLoop :: [Inst] -> [Inst] -> StackM ()
+execLoop testInsts bodyInsts = do
+  mapM_ evalInst testInsts
+  v <- pop
+  if (toBool v) 
+    then mapM_ evalInst bodyInsts >> execLoop testInsts bodyInsts
+    else return ()
+         
 {- holy code duplication batman 
    Also, we want pop to return the integer value that corresponds to the variable. 
 
   Loops are complicated: I'm thinking that loops are actually going to need to be done as chickens^W, essentially, a "zipper" structure that will keep around the context of other code and then we can actually hold onto those future instructions as a "loop stack" essentially until the first element of the loop works
+
+    MEH actually I'm thinking that loops will just be done by having a separate set of instructions held as part of the loop. It breaks the "linearity" of it, but that's kinda what loops do --- they're not a linear execution, so I think it's okay that it's handled differently.
+
    -}
 
-{- to fix
+
 topEval :: [Inst] -> Int
-topEval ts = last $ evalState (mapM evalInst ts) ([],M.empty,[])
--}
+topEval ts = last $ lefts $ fst $ (execState (mapM_ evalInst ts) ([],M.empty))
